2137a3e7376063a724ce0ca592f2e9c344d8b241
Justice <sherllyj191@gmail.com>
2025-06-05 05:37:21 +0100

    changes

--- contracts/fundme/FundMe.sol
+++ contracts/fundme/FundMe.sol
@@ -1,27 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// @ts-ignore
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
   address private sepoliaAddress = address(0x694AA1769357215DE4FAC081bf1f309aDC325306); // Sepolia address for ETH/USD price feed
 
   // uint256 public minimumUSD = 50 * 10**18; // 50 USD in wei
-  uint256 public minimumUSD = 5;
+  uint256 public minimumUSD = 5e18;
 
   function fund() public payable {
-    require(msg.value >= minimumUSD, "You need to spend more ETH!");
+    require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
   }
 
   function getLatestPrice() public view returns (uint256) {
     AggregatorV3Interface priceFeed = AggregatorV3Interface(sepoliaAddress);
     (, int price, , , ) = priceFeed.latestRoundData();
-    return convertUsdToWei(price);
-  }
-
-  function convertUsdToWei(int256 usdAmount) private pure returns (uint256) {
     // USD has 8 decimals, ETH has 18 decimals
     // To convert USD to wei, we multiply by 10^10 (to adjust for the difference in decimals)
-    return uint256(usdAmount * 1e10);
+    return uint256(price * 1e10);
+  }
+
+  function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+    uint256 ethPrice = getLatestPrice();
+    uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18; // Convert ETH to USD
+    return ethAmountInUSD;
   }
 }
